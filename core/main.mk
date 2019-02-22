3648697abe04ff1880ea8212df59eb6fafcc3aa9
tuxafgmur <tuxafgmur@users.noreply.github.com>
2019-02-22 04:54:26 +0100

    main: Changes to adb secure

--- core/main.mk
+++ core/main.mk
@@ -284,7 +284,7 @@ ifneq (,$(user_variant))
 
   ifeq ($(user_variant),userdebug)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
-    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+    ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
     ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
     tags_to_install += debug
   else
